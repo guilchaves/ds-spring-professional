0cfcf6b57926b8b11d3d77a61f48240d077a938b
guilherme <chaves.guilherme@proton.me>
2023-12-01 20:45:55 -0300

    chore: add command prompt

--- src/main/java/br/com/chaves/desafio/DesafioApplication.java
+++ src/main/java/br/com/chaves/desafio/DesafioApplication.java
@@ -1,28 +1,43 @@
 package br.com.chaves.desafio;
 
 import br.com.chaves.desafio.entities.Order;
+import br.com.chaves.desafio.services.OrderService;
 import br.com.chaves.desafio.services.ShippingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 @SpringBootApplication
 public class DesafioApplication implements CommandLineRunner {
 
-	@Autowired
-	public ShippingService shippingService;
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(DesafioApplication.class, args);
-	}
+    @Autowired
+    public OrderService orderService;
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("\nHello world!");
-		Order o = new Order(1309, 230.00, 20.00);
+    @Override
+    public void run(String... args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
-		System.out.println(shippingService.shipment(o));
+        System.out.print("Entre com o código do pedido: ");
+        Integer code = sc.nextInt();
+        System.out.print("Entre com o valor básico do pedido: ");
+        Double basic = sc.nextDouble();
+        System.out.print("Entre com a porcentagem de desconto: ");
+        Double discount = sc.nextDouble();
 
-	}
+        Order o = new Order(code, basic, discount);
+        System.out.println("Pedido código " + o.getCode());
+        System.out.println("Valor total: R$ " + String.format("%.2f", orderService.total(o)));
+
+
+        sc.close();
+    }
 }
