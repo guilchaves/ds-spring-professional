a29f4abac798c342450e9fe9fbdb7cdeb74ced03
guilherme <chaves.guilherme@proton.me>
2023-12-01 19:20:33 -0300

    chore: section01 demo exercise 01

--- section01/class/src/test/java/com/chaves/aula/AulaApplicationTests.java
+++ section01/class/src/test/java/com/chaves/aula/AulaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.chaves.aula;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ClassApplicationTests {
+class AulaApplicationTests {
 
 	@Test
 	void contextLoads() {
