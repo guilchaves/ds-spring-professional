8374378b1030bd52df64bcb496d4784c19856171
guilherme <chaves.guilherme@proton.me>
2023-12-04 20:47:08 -0300

    feat: add Order entity, add OrderItem entity

--- project/dscommerce/src/main/java/br/com/guilchaves/dscommerce/entities/Product.java
+++ project/dscommerce/src/main/java/br/com/guilchaves/dscommerce/entities/Product.java
@@ -3,6 +3,7 @@ package br.com.guilchaves.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -20,9 +21,12 @@ public class Product {
 
     @ManyToMany
     @JoinTable(name = "tb_product_category",
-            joinColumns =  @JoinColumn(name = "product_id"),
+            joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -77,5 +81,13 @@ public class Product {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Order> getOrders() {
+        return items.stream().map(x -> x.getOrder()).toList();
     }
 }
