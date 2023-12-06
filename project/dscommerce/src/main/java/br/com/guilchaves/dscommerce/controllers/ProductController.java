0e03dea15d3144fe3e33596c5371d5ee22a62361
guilherme <chaves.guilherme@proton.me>
2023-12-06 15:47:00 -0300

    feat: add use case insert product

--- project/dscommerce/src/main/java/br/com/guilchaves/dscommerce/controllers/ProductController.java
+++ project/dscommerce/src/main/java/br/com/guilchaves/dscommerce/controllers/ProductController.java
@@ -5,10 +5,7 @@ import br.com.guilchaves.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -19,12 +16,17 @@ public class ProductController {
     private ProductService service;
 
     @GetMapping
-    public Page<ProductDTO> findAll(Pageable pageable){
+    public Page<ProductDTO> findAll(Pageable pageable) {
         return service.findAll(pageable);
     }
 
     @GetMapping(value = "/{id}")
     public ProductDTO findById(@PathVariable Long id) {
         return service.findById(id);
+    }
+
+    @PostMapping
+    public ProductDTO insert(@RequestBody ProductDTO dto) {
+        return service.insert(dto);
     }
 }
