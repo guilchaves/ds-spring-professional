101b3d07e52b7abf201483c8e9d2f6796e594dea
guilherme <chaves.guilherme@proton.me>
2023-12-06 19:45:13 -0300

    feat: add bean validation

--- project/dscommerce/src/main/java/br/com/guilchaves/dscommerce/dto/ProductDTO.java
+++ project/dscommerce/src/main/java/br/com/guilchaves/dscommerce/dto/ProductDTO.java
@@ -1,12 +1,20 @@
 package br.com.guilchaves.dscommerce.dto;
 
 import br.com.guilchaves.dscommerce.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
 
     private Long id;
+    @Size(min = 3, max = 80, message = "Name must be between {min} and {max} characters long")
+    @NotBlank(message = "Name is required")
     private String name;
+    @Size(min = 10, message = "Description must be at least {min} characters")
+    @NotBlank(message = "Description is required")
     private String description;
+    @Positive(message = "Price must be a positive value")
     private Double price;
     private String imgUrl;
 
