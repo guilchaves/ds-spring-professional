bb640f1b699658ba388cf5e470a774779cacf2d0
guilherme <chaves.guilherme@proton.me>
2023-12-07 13:15:17 -0300

    feat: implement client findById and Insert client

--- src/main/java/br/com/guilchaves/desafio/controllers/ClientController.java
+++ src/main/java/br/com/guilchaves/desafio/controllers/ClientController.java
@@ -6,10 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -19,9 +19,26 @@ public class ClientController {
     private ClientService service;
 
     @GetMapping
-    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable){
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
         Page<ClientDTO> dto = service.findAll(pageable);
         return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        ClientDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(dto.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 
 }
--- src/main/java/br/com/guilchaves/desafio/services/ClientService.java
+++ src/main/java/br/com/guilchaves/desafio/services/ClientService.java
@@ -23,5 +23,26 @@ public class ClientService {
         return result.map(ClientDTO::new);
     }
 
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id){
+        Client client = repository.findById(id).get();
+        return new ClientDTO(client);
+    }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto){
+        Client entity = new Client();
+        copyPropertiesToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    private void copyPropertiesToEntity(ClientDTO dto, Client entity) {
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
+    }
 
 }
