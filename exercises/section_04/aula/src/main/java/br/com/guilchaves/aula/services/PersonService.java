5f2f0da9785cf2a4e522a9106a154d87e5dbbe13
guilherme <chaves.guilherme@proton.me>
2023-12-09 13:13:14 -0300

    feat: retrieve department name on entity

--- exercises/section_04/aula/src/main/java/br/com/guilchaves/aula/services/PersonService.java
+++ exercises/section_04/aula/src/main/java/br/com/guilchaves/aula/services/PersonService.java
@@ -3,6 +3,7 @@ package br.com.guilchaves.aula.services;
 import br.com.guilchaves.aula.dto.PersonDepartmentDTO;
 import br.com.guilchaves.aula.entities.Department;
 import br.com.guilchaves.aula.entities.Person;
+import br.com.guilchaves.aula.repositories.DepartmentRepository;
 import br.com.guilchaves.aula.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,19 +12,23 @@ import org.springframework.stereotype.Service;
 public class PersonService {
 
     @Autowired
-    private PersonRepository repository;
+    private PersonRepository personRepository;
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
 
     public PersonDepartmentDTO insert(PersonDepartmentDTO dto){
         Person entity = new Person();
         entity.setName(dto.getName());
         entity.setSalary(dto.getSalary());
 
-        Department department = new Department();
-        department.setId(dto.getDepartment().getId());
+        Department department = departmentRepository.getReferenceById(dto.getDepartment().getId());
+
+        //Department dept = new Department();
+        //dept.setId(dto.getDepartment().getId());
 
         entity.setDepartment(department);
-
-        entity = repository.save(entity);
+        entity = personRepository.save(entity);
         return new PersonDepartmentDTO(entity);
     }
 
