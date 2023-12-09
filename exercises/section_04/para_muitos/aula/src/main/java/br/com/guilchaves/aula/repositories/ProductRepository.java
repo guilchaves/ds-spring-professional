3cad19381f7920bb2db3add7220106ad73be19d6
guilherme <chaves.guilherme@proton.me>
2023-12-09 17:15:01 -0300

    fix: initial project for toMany class

--- exercises/section_04/para_muitos/aula/src/main/java/br/com/guilchaves/aula/repositories/ProductRepository.java
+++ exercises/section_04/para_muitos/aula/src/main/java/br/com/guilchaves/aula/repositories/ProductRepository.java
@@ -1,6 +1,6 @@
 package br.com.guilchaves.aula.repositories;
 
-import br.com.guilchaves.aula.entities.Category;
+import br.com.guilchaves.aula.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
