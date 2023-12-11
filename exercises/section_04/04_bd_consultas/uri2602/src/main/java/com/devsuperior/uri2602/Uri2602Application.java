ad2e2cf56580732d51971d4130c9adb292e72d66
guilherme <chaves.guilherme@proton.me>
2023-12-11 17:53:32 -0300

    feat: add sql solution to uri 2602

--- exercises/section_04/04_bd_consultas/uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
+++ exercises/section_04/04_bd_consultas/uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
@@ -1,12 +1,30 @@
 package com.devsuperior.uri2602;
 
+import com.devsuperior.uri2602.dto.CustomerMinDTO;
+import com.devsuperior.uri2602.projections.CustomerMinProjection;
+import com.devsuperior.uri2602.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Uri2602Application {
+import java.util.List;
+import java.util.stream.Collectors;
 
-	public static void main(String[] args) {
-		SpringApplication.run(Uri2602Application.class, args);
-	}
+@SpringBootApplication
+public class Uri2602Application implements CommandLineRunner {
+
+    @Autowired
+    private CustomerRepository repository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Uri2602Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        List<CustomerMinProjection> list = repository.filterByState("RS");
+        List<CustomerMinDTO> result1 = list.stream().map(CustomerMinDTO::new).collect(Collectors.toList());
+        result1.forEach(System.out::println);
+    }
 }
