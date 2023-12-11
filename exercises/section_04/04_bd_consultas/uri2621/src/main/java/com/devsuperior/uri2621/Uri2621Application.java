8b3b8452c9b3dea8d3b706f55f971d5fd6c44cba
guilherme <chaves.guilherme@proton.me>
2023-12-11 20:42:17 -0300

    feat: finish case study simple join with between

--- exercises/section_04/04_bd_consultas/uri2621/src/main/java/com/devsuperior/uri2621/Uri2621Application.java
+++ exercises/section_04/04_bd_consultas/uri2621/src/main/java/com/devsuperior/uri2621/Uri2621Application.java
@@ -1,11 +1,15 @@
 package com.devsuperior.uri2621;
 
+import com.devsuperior.uri2621.dto.ProductMinDTO;
+import com.devsuperior.uri2621.projections.ProductMinProjection;
+import com.devsuperior.uri2621.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.devsuperior.uri2621.repositories.ProductRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Uri2621Application implements CommandLineRunner {
@@ -19,6 +23,16 @@ public class Uri2621Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
+		List<ProductMinProjection> list = repository.filterByAmount(10, 20, "P");
+		List<ProductMinDTO> resultSql = list.stream().map(ProductMinDTO::new).collect(Collectors.toList());
+		System.out.println("*** RESULTADO SQL ***");
+		resultSql.forEach(System.out::println);
+
+
+		System.out.println("\n\n");
+		List<ProductMinDTO> resultJpql = repository.filterByAmount2(10, 20, "P");
+		System.out.println("*** RESULTADO JPQL ***");
+		resultJpql.forEach(System.out::println);
 	}
 }
