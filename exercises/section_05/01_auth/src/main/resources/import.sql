bbca01304c5e843039cc87bbb0be5fe3d6a082cf
guilherme <chaves.guilherme@proton.me>
2023-12-14 18:03:57 -0300

    feat: add BCrypt password

--- exercises/section_05/01_auth/src/main/resources/import.sql
+++ exercises/section_05/01_auth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$.b7kwNIK0.LgKORXat067um7QHYqk00j2W1auI805SWv4E.ckPgIS');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$.b7kwNIK0.LgKORXat067um7QHYqk00j2W1auI805SWv4E.ckPgIS');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
