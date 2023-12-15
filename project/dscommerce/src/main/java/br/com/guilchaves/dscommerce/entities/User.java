fa9df1657792a2f917522b5a37b690dd79270fc2
guilherme <chaves.guilherme@proton.me>
2023-12-15 11:57:50 -0300

    feat: add user projection, repository and service

--- project/dscommerce/src/main/java/br/com/guilchaves/dscommerce/entities/User.java
+++ project/dscommerce/src/main/java/br/com/guilchaves/dscommerce/entities/User.java
@@ -88,6 +88,10 @@ public class User implements UserDetails {
         return orders;
     }
 
+    public void addRole(Role role){
+        roles.add(role);
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return roles;
